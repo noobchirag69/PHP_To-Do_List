2e713751a3604b48cc9fb0c3be8c3b0f454ba641
noobchirag69 <chiragchakraborty48@gmail.com>
2022-10-26 13:29:15 +0530

    Second Commit

--- about.php
+++ about.php
@@ -7,9 +7,8 @@
     <main>
         <div class="about-container">
             <h1>About</h1>
-            <p>I made this project for my 3rd semester. Feel free to use it. Star the GitHub repository if you like it.
-                Thanks for visiting!</p>
-            <p><a href="#">Source Code</a></p>
+            <p>Version 1.0.0</p>
+            <p><a href="https://github.com/noobchirag69/PHP_To-Do_List" target="_blank">Source Code</a></p>
         </div>
     </main>
     <!-- Footer -->
